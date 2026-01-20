b59f8ad10356071b88d8970a3409b03292836e7d
Aryesh Saha <aryesh05.works@gmail.com>
2026-01-20 11:43:36 +0530

    added mvn tool

--- src/test/java/TestRunner/StepRunner.java
+++ src/test/java/TestRunner/StepRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/feature/01_signup.feature", "src/test/resources/feature/02_login.feature","src/test/resources/feature/admin/03_subjectAdd.feature", "src/test/resources/feature/admin/04_subtopicAdd.feature"},
-        glue = {"StepDefinition", "StepDefinition/admin"},
+        glue = {"StepDefinition"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html"
